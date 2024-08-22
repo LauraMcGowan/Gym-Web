5757fcb26fe13ea0f200de81a4480a7fe9c1ab6f
Laura McGowan <laura.mcgowan@bbc.co.uk>
2024-08-22 09:01:33 +0100

    feat:

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import logo from "./assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-        <img src={logo} alt="img" />
+    return (
+      <div>
+          <img src={logo} alt="img" />
+          <br />
+          <ul>
+              <li>
+                  {/* Endpoint to route to Home component */}
+                  <Link to="/">Home</Link>
+              </li>
+              <li>
+                  {/* Endpoint to route to About component */}
+                  <Link to="/meettheteam">Meet The Team</Link>
+              </li>
+              <li>
+                  {/* Endpoint to route to Contact Us component */}
+                  <Link to="/contactus">Contact Us</Link>
+              </li>
+          </ul>
+      </div>
   );
+     
+
 };
 
 export default Home;
