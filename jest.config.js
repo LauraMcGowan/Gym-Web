a5dcb7bde3d5b26c94fcc7bc42fa8ad726a02ae4
Laura McGowan <laura.mcgowan@bbc.co.uk>
2024-11-15 16:34:43 +0000

    chore: :package: updated dep

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleFileExtensions: ["js", "jsx"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // For mocking CSS imports
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+
+    // "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js", // For mocking image imports
+  },
+
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
 };
